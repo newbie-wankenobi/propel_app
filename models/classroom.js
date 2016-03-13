452be39618f18bdf9d69f4b01cb1dfc8b3900667
Michael Duran <mad2116@gmail.com>
2016-03-13 01:50:14 -0800

    update classroom model

--- models/classroom.js
+++ models/classroom.js
@@ -2,34 +2,38 @@ var mongoose = require('mongoose'),
     debug    = require('debug')('app:models');
 
 var commentSchema = new mongoose.Schema({
-  author: {},
-  body: {},
-  createdAt: {},
-  comments: [this]
+  author:    {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  body:      {type: String, required: true},
+  createdAt: {type: Date, default: Date.now},
+  // comments: [this]
 });
 
 var answerSchema = new mongoose.Schema({
-  author: {},
-  body: {},
-  createdAt: {},
-  comments: [commentSchema]
+  author:    {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  body:      {type: String, required: true},
+  createdAt: {type: Date, default: Date.now},
+  comments:  [commentSchema]
 });
 
 var questionSchema = new mongoose.Schema({
-  author: {},
-  body: {},
-  createdAt: {},
+  author:    {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  body:      {type: String, required: true},
+  createdAt: {type: Date, default: Date.now},
   answers: [answerSchema]
 });
 
 var classroomSchema = new mongoose.Schema({
-  type: {},
-  name: {},
-  creator: {},
-  admins: {},
-  students: {},
-  professionals: {},
-  questions: {}
+  type:          {type: String, required: true, enum: ['course', 'subject-thread', 'industry-thread'], default: 'course'},
+  name:          {type: String, required: true},
+  createdAt:     {type: Date, default: Date.now},
+  creator:       {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
+  admins:        [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+  students:      [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+  professionals: [{type: mongoose.Schema.Types.ObjectId, ref: 'User'}],
+  questions:     [questionSchema]
+  // schedule:   {days: [String], startDate: Date, endDate: Date, startTime: Number, endTime: Number},
+  // syllabus:   String,
+  // lessons:    [String],
 });
 
 var Classroom = mongoose.model('Classroom', classroomSchema);
