d2b6e0abb628f490b4db3f91a7ff18f9ae707237
Michael Duran <mad2116@gmail.com>
2016-03-12 21:57:43 -0800

    update user model

--- models/user.js
+++ models/user.js
@@ -2,12 +2,14 @@ var mongoose = require('mongoose'),
     debug    = require('debug')('app:models');
 
 var userSchema = new mongoose.Schema({
-  type: String,
-  name:   String,
-  lastName: String,
-  handle: String,
-  email: String,
-  institution: String,
+  type:                     {type: String, required: true, enum: ['student', 'instructor', 'professional'], default: 'student'},
+  linkedInId:               {type: String, required: true, default: null},
+  linkedInPublicProfileUrl: {type: String, required: true, default: null},
+  firstName:                {type: String, required: true},
+  lastName:                 {type: String, required: true},
+  emailAddress:             {type: String, required: true},
+  institution:              {type: String, required: true},
+  location:                 {name: String, countryCode: String}
 });
 
 var User = mongoose.model('User', userSchema);
