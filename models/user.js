4cb4e22ca0198ddc766b1d84a4bc89e919ab0ec7
Edward Estese <tonyestese@hotmail.com>
2016-03-13 20:35:02 -0700

    Oauth configuration
    add .env, passport.js, defined routes for Oauth
    modified users model for Oauth,

--- models/user.js
+++ models/user.js
@@ -2,9 +2,22 @@ var mongoose = require('mongoose'),
     debug    = require('debug')('app:models');
 
 var userSchema = new mongoose.Schema({
-  name:   String,
-  handle: String
+  type:                     {type: String, required: true, enum: ['student', 'instructor', 'professional'], default: 'student'},
+  linkedInId:               {type: String, required: true, default: 'LINKEDIN_ID'},
+  linkedInPublicProfileUrl: {type: String, required: true, default: 'LINKEDIN_PROFILE_URL'},
+  firstName:                {type: String, required: true},
+  lastName:                 {type: String, required: true},
+  email:                    {type: String, required: true},
+  institution:              {type: String},
+  location:                 {type: String, countryCode: String}
 });
+
+// creating an instance method for the userSchema
+// http://mongoosejs.com/docs/guide.html#methods
+userSchema.methods.isStudent = function() {
+  if (this.type === 'student') return true;
+  return false;
+};
 
 var User = mongoose.model('User', userSchema);
 
