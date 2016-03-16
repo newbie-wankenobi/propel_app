b3f786bfa3e1bab08dd46654c4013e63427e402f
Keith To <keithtkto@gmail.com>
2016-03-16 15:24:39 -0700

    rendering question works

--- public/js/spike_questions_render.js
+++ public/js/spike_questions_render.js
@@ -5,18 +5,16 @@ console.log('spike_questions_render loaded');
 
 var $questionListEl; //<section> of where the question get posted
 var renderQuestions = _.template(`
-      <article id="<$= _id $>" class="">
-        <a href="/users/<$= _id $>"><h3><$= name $></h3></a>
-        // <a href="/users/<$= creator._id $>"><h3><$= creator.name $></h3></a>
-        <p><$= description $></p>
-        <span>
-          Created at <$ createdAt $>, by
-          <$= creator.displayName() $>
-        </span>
-    // <!-- <br> -->
-    // <!-- <button>upvote</button> VoteCount <button>downvote</button> -->
-    // <!-- <br> -->
-    // <!-- <button>delete this question (only delete user own question)</button> -->
+      <article id="<%= _id %>" class="">
+        <div class="question-title">
+          <a href="/users/<%= _id %>"><h3><%= title %></h3></a>
+          <h6> Asked by<a href="/users/<%= author %>"> %%= author fullname %%, <%= createdAt %></h6></a>
+        </div>
+        <p><%= body %></p>
+        <!-- <br> -->
+        <!-- <button>upvote</button> VoteCount <button>downvote</button> -->
+        <!-- <br> -->
+        <!-- <button>delete this question (only delete user own question)</button> -->
       </article>
     `);
 
@@ -24,28 +22,31 @@ var renderQuestions = _.template(`
 function postQuestions(question){
   var questionComponent  = renderQuestions(question);
   var $questionComponent = $(questionComponent);
-      $questionListEl    = $('#questionList');
+      $questionListEl    = $('#question-list');
 
       $questionListEl.append($questionComponent);
 }
 
  //$questionList = <section> of where the question get posted}
 
-function indexingClassrooms() {
+function indexingQuestions() {
+  var classId = "56e9b41fcb3b16c659ce662c";
   $.ajax({
     method: "GET",
-    url: "api/classrooms"
-  }).then(function(classrooms){
-    console.log("RENDERING Q", questions);
+    url: "api/classrooms/" + classId,
+  }).then(function(classroom){
+    console.log("RENDERING Classrooms", classroom);
 
     classroom.questions.forEach(function(question){
       postQuestions(question);
     });
-
-
   })
-
 }
+
+$( document ).ready( function() {
+  indexingQuestions();
+  }
+);
 
 
 
